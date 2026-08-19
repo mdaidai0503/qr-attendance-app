00f5193c649154f2fcf944a1600cd5956cb6e16e
mdaidai0503 <mdaidai0503@gmail.com>
2026-08-19 18:04:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='qr-attendance-v2.1-fixedqr-required';
+const CACHE='qr-attendance-v2.2-auto-supabase';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
